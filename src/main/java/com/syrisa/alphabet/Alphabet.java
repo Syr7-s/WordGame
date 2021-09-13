6250d3c31f2cc469111e2e00ab828d35337e9cda
Syr7-s <isasayar7@github.com>
2021-09-13 22:04:02 +0300

    Alphabet class was filled.

--- src/main/java/com/syrisa/alphabet/Alphabet.java
+++ src/main/java/com/syrisa/alphabet/Alphabet.java
@@ -1,4 +1,14 @@
 package com.syrisa.alphabet;
 
 public class Alphabet {
+    private static Alphabet instance = null;
+    private static final Object LOCK = new Object();
+
+    private Alphabet(){}
+
+    public static Alphabet getInstance(){
+        synchronized (LOCK){
+            return instance == null ? instance = new Alphabet() : instance;
+        }
+    }
 }
