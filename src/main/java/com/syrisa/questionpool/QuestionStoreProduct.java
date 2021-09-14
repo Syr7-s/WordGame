7334b3b416059138cc912d78d1375ff853b9787b
Syr7-s <isasayar7@github.com>
2021-09-14 20:30:28 +0300

    QuestionPoolService, QuestionStoreProduct and QuestionStoreProductService were edited.

--- src/main/java/com/syrisa/questionpool/QuestionStoreProduct.java
+++ src/main/java/com/syrisa/questionpool/QuestionStoreProduct.java
@@ -1,4 +1,21 @@
 package com.syrisa.questionpool;
 
+import com.syrisa.question.Question;
+import com.syrisa.questionpool.service.QuestionStoreProductService;
+
+import java.util.ArrayList;
+
 public class QuestionStoreProduct {
+    QuestionStoreProductService storeProduct = type ->{
+        switch (type){
+            case "Actor":
+            case "Actress":
+            case "Sportier":
+            case "Movie":
+            case "Single":
+                return new ArrayList<>();
+            default:
+                throw new RuntimeException("Exception Occurred");
+        }
+    };
 }
