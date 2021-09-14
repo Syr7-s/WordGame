673ccca9fe16e07653a0ef5107c51704c1a042fb
Syr7-s <isasayar7@github.com>
2021-09-14 20:22:48 +0300

    addQuestionToPool method was added.

--- src/main/java/com/syrisa/questionpool/service/QuestionStoreProductService.java
+++ src/main/java/com/syrisa/questionpool/service/QuestionStoreProductService.java
@@ -1,4 +1,10 @@
 package com.syrisa.questionpool.service;
 
+import com.syrisa.question.Question;
+
+import java.util.List;
+import java.util.Map;
+
 public interface QuestionStoreProductService {
+    Map<String,List<Question>> addQuestionToPool(Question question);
 }
