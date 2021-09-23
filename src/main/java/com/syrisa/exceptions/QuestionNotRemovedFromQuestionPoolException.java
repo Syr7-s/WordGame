b9b759bdb3edb1c32539ba2142ee6eb8593dfb09
Syr7-s <isasayar7@github.com>
2021-09-23 20:09:16 +0300

    QuestionNotRemovedFromQuestionPoolException was created.

---
+++ src/main/java/com/syrisa/exceptions/QuestionNotRemovedFromQuestionPoolException.java
@@ -0,0 +1,7 @@
+package com.syrisa.exceptions;
+
+public class QuestionNotRemovedFromQuestionPoolException extends Exception {
+    public QuestionNotRemovedFromQuestionPoolException(String message) {
+        super(message);
+    }
+}
